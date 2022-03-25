43caaed55602026d2135f85c0976401f5d7aec92
Ankushtsalian <ankushtsalian@gmail.com>
2022-03-25 11:57:28 +0530

    outline of home page

--- shoppingmart/src/App.js
+++ shoppingmart/src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
+  const categories = [
+    { id: 1, title: "Hats" },
+    { id: 2, title: "Jackets" },
+    { id: 3, title: "Sneakers" },
+    { id: 4, title: "Women" },
+    { id: 5, title: "men" },
+  ];
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="catogories-container">
+      {categories.map(({ title }) => (
+        <div className="catogory-container">
+          {/* <img/> */}
+          <div className="catogory-body-container">
+            <h2>{title}</h2>
+            <p>shop Now</p>
+          </div>
+        </div>
+      ))}
     </div>
   );
 }
