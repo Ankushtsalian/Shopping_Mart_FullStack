4a87d92a8c3f4d2d03fed813f0e30794cbbb13b0
Ankushtsalian <ankushtsalian@gmail.com>
2022-03-25 16:36:14 +0530

    component dismantled and created Categorymenu and added scss styles to container

--- shoppingmart/src/components/category-item/category-item.js
+++ shoppingmart/src/components/category-item/category-item.js
@@ -1,7 +1,21 @@
 import React from "react";
 import "./category-item.styles.scss";
-const categoryItem = () => {
-  return <div>category-item</div>;
+const Categoryitem = (props) => {
+  const {
+    category: { title, id, imageUrl },
+  } = props;
+  return (
+    <div key={id} className="category-container">
+      <div
+        className="background-image"
+        style={{ backgroundImage: `url(${imageUrl})` }}
+      />
+      <div className="category-body-container">
+        <h2>{title}</h2>
+        <p>shop Now</p>
+      </div>
+    </div>
+  );
 };
 
-export default categoryItem;
+export default Categoryitem;
