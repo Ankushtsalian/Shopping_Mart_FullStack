f54476080c971996f8bd32c01ffd862603f1d1a7
Ankushtsalian <ankushtsalian@gmail.com>
2022-03-26 14:53:17 +0530

    Navigation bar comp with roter setup

--- shoppingmart/src/routes/navigation/Navigation.js
+++ shoppingmart/src/routes/navigation/Navigation.js
@@ -1,11 +1,23 @@
-import { Outlet } from "react-router-dom";
+import { Link, Outlet } from "react-router-dom";
 
 const Navbar = () => {
   return (
-    <div>
-      App
+    <>
+      <div className="navigation">
+        <Link className="logo-container" to="/">
+          <div> Logo </div>
+        </Link>
+
+        <div className="nav-links-container">
+          <Link className="nav-link" to="/shop">
+            SHOP
+          </Link>
+        </div>
+
+        <h1>N A V I G A T I O N</h1>
+      </div>
       <Outlet />
-    </div>
+    </>
   );
 };
 
