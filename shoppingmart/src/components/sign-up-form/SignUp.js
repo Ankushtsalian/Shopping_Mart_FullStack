db1a5738379d1937a524eec5822eba3e888c9592
Ankushtsalian <ankushtsalian@gmail.com>
2022-03-27 17:18:09 +0530

    signUp form created and forn states are created

--- shoppingmart/src/components/sign-up-form/SignUp.js
+++ shoppingmart/src/components/sign-up-form/SignUp.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 const defaultFormsFields = {
   displayName: "",
@@ -6,22 +6,55 @@ const defaultFormsFields = {
   password: "",
   confirmPassword: "",
 };
+
 const SignUp = () => {
+  const [formsFields, setFormsFields] = useState(defaultFormsFields);
+  const { displayName, email, password, confirmPassword } = formsFields;
+
+  console.log(formsFields);
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setFormsFields((prev) => ({ ...prev, [name]: value }));
+  };
   return (
     <div>
       <h1>Sign Up with Your Email and Password</h1>
-      <form onSubmit={() => {}}>
+      <form
+        onSubmit={() => {
+          //   handleSubmit;
+        }}
+      >
         <label>Display Name</label>
-        <input type="text" />
+        <input
+          type="text"
+          onChange={handleChange}
+          name="displayName"
+          value={displayName}
+        />
 
         <label>Email</label>
-        <input type="email" />
+        <input
+          type="email"
+          onChange={handleChange}
+          name="email"
+          value={email}
+        />
 
         <label>Password</label>
-        <input type="password" />
+        <input
+          type="password"
+          onChange={handleChange}
+          name="password"
+          value={password}
+        />
 
         <label>Confirm Password</label>
-        <input type="password" />
+        <input
+          type="password"
+          onChange={handleChange}
+          name="confirmPassword"
+          value={confirmPassword}
+        />
 
         <button type="submit">SignUp</button>
       </form>
