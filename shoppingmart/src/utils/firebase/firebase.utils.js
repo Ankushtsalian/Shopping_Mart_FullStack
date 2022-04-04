73809b323cbf5ffcbd578daf3c89fe22d42604d5
Ankushtsalian <ankushtsalian@gmail.com>
2022-04-04 19:04:28 +0530

    added categories to db using firestore methods collection,writeBatch

--- shoppingmart/src/utils/firebase/firebase.utils.js
+++ shoppingmart/src/utils/firebase/firebase.utils.js
@@ -13,7 +13,15 @@ import {
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
-import { getFirestore, doc, getDoc, setDoc } from "firebase/firestore";
+import {
+  getFirestore,
+  doc,
+  getDoc,
+  setDoc,
+  collection,
+  WriteBatch,
+  writeBatch,
+} from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -51,6 +59,21 @@ export const signInWithGoogleRedirect = () =>
 
 //userAuth from signin as arg
 export const db = getFirestore();
+
+export const addCollectionAndDocuments = async (
+  collectionKey,
+  objectsToAdd
+) => {
+  const collectionRef = collection(db, collectionKey);
+  const batch = writeBatch(db);
+
+  objectsToAdd.forEach((object) => {
+    const docRef = doc(collectionRef, object.title.toLowerCase());
+    batch.set(docRef, object);
+  });
+  await batch.commit();
+  console.log("done");
+};
 
 export const createUserDocumentFromAuth = async (
   userAuth,
