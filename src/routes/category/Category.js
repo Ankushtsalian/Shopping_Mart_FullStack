6a137859bb53deb8496e0ba2e6cc65809f8d83c5
Ankushtsalian <ankushtsalian@gmail.com>
2022-04-07 21:21:33 +0530

    build

---
+++ src/routes/category/Category.js
@@ -0,0 +1,29 @@
+import React, { useContext, useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import ProductCard from "../../components/ProductCard/ProductCard";
+import { CategoriesContext } from "../../contexts/categories.context";
+import "./category.styles.scss";
+const Category = () => {
+  const { categoriesMap } = useContext(CategoriesContext);
+  const { category } = useParams();
+  // const [products, setProducts] = useState(categoriesMap[category]);
+  const products = categoriesMap[category];
+
+  // useEffect(() => {
+  //   setProducts(categoriesMap[category]);
+  // }, [categoriesMap, category]);
+
+  return (
+    <>
+      <h2 className="category-title">{category.toUpperCase()}</h2>
+      <div className="category-products-container">
+        {products &&
+          products.map((product) => (
+            <ProductCard key={product.id} product={product} />
+          ))}
+      </div>
+    </>
+  );
+};
+
+export default Category;
