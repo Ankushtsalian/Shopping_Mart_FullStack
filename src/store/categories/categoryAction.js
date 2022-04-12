a37f42831933bde245254572bd2ed1bbc7bbb21a
Ankushtsalian <ankushtsalian@gmail.com>
2022-04-12 19:13:00 +0530

    UPDATED fetchCategoriesAsync in action

--- src/store/categories/categoryAction.js
+++ src/store/categories/categoryAction.js
@@ -1,5 +1,25 @@
+import { getCategoriesAndDocuments } from "../../utils/firebase/firebase.utils";
 import { createAction } from "../../utils/reducer/reducer.utils";
 import { CATEGORIES_ACTION_TYPES } from "./categoryTypes";
 
 export const setCategoriesMap = (categoriesMap) =>
   createAction(CATEGORIES_ACTION_TYPES.SET_CATEGORIES_MAP, categoriesMap);
+
+export const fetchCategoriesStart = () =>
+  createAction(CATEGORIES_ACTION_TYPES.FETCH_CATEGORY_START);
+
+export const fetchCategoriesSuccess = (categoriesMap) =>
+  createAction(CATEGORIES_ACTION_TYPES.FETCH_CATEGORY_SUCCESS, categoriesMap);
+
+export const fetchCategoriesFailed = (error) =>
+  createAction(CATEGORIES_ACTION_TYPES.FETCH_CATEGORY_FAILED, error);
+
+export const fetchCategoriesAsync = () => async (dispatch) => {
+  dispatch(fetchCategoriesStart());
+  try {
+    const categoryMap = await getCategoriesAndDocuments();
+    dispatch(fetchCategoriesSuccess(categoryMap));
+  } catch (error) {
+    dispatch(fetchCategoriesFailed(error));
+  }
+};
