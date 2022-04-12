16bd80ce717dd13578a005b90072a47c00fe823d
Ankushtsalian <ankushtsalian@gmail.com>
2022-04-12 18:50:33 +0530

    UPDATed cartReducer wrt ACTIONS

--- src/store/categories/categoryReducer.js
+++ src/store/categories/categoryReducer.js
@@ -2,14 +2,24 @@ import { CATEGORIES_ACTION_TYPES } from "./categoryTypes";
 
 export const CATEGORIES_INITIAL_STATE = {
   categoriesMap: {},
+  isLoading: false,
+  error: null,
 };
-
+// FETCH_CATEGORY_START: "category/FETCH_CATEGORY_START",
+// FETCH_CATEGORY_SUCCESS: "category/FETCH_CATEGORY_SUCCESS",
+// FETCH_CATEGORY_FAILED: "category/FETCH_CATEGORY_FAILED",
 export const categoriesReducer = (state = CATEGORIES_INITIAL_STATE, action) => {
   const { type, payload } = action;
 
   switch (type) {
-    case CATEGORIES_ACTION_TYPES.SET_CATEGORIES_MAP:
-      return { ...state, categoriesMap: payload };
+    case CATEGORIES_ACTION_TYPES.FETCH_CATEGORY_START:
+      return { ...state, isLoading: true };
+
+    case CATEGORIES_ACTION_TYPES.FETCH_CATEGORY_SUCCESS:
+      return { ...state, categoriesMap: payload, isLoading: false };
+
+    case CATEGORIES_ACTION_TYPES.FETCH_CATEGORY_FAILED:
+      return { ...state, error: payload, isLoading: false };
 
     default:
       return state;
