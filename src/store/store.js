9db9a120f72fc65c8f8a051f8aedd4e7c3d0c66e
Ankushtsalian <ankushtsalian@gmail.com>
2022-04-12 18:35:51 +0530

    added thunk in store

--- src/store/store.js
+++ src/store/store.js
@@ -1,8 +1,8 @@
-import { createStore } from "redux";
+import { createStore, compose, applyMiddleware } from "redux";
 import { persistStore, persistReducer } from "redux-persist";
 import { rootReducer } from "./root-reducer";
 import storage from "redux-persist/lib/storage";
-
+import thunk from "redux-thunk";
 const persistConfig = {
   key: "root",
   storage,
@@ -10,13 +10,47 @@ const persistConfig = {
 };
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);
+const middlewares = [process.env.NODE_ENV !== "production" && thunk].filter(
+  Boolean
+);
+
+const composeEnhancer =
+  (process.env.NODE_ENV !== "production" &&
+    window &&
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) ||
+  compose;
+
+const composedEnhancers = composeEnhancer(applyMiddleware(...middlewares));
+// export const store = createStore(rootReducer, undefined, composedEnhancers);
+
 export const store = createStore(
   persistedReducer,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  undefined,
+  composedEnhancers
 );
 
 export const persistor = persistStore(store);
+// import { createStore } from "redux";
+// import { persistStore, persistReducer } from "redux-persist";
+// import { rootReducer } from "./root-reducer";
+// import storage from "redux-persist/lib/storage";
 
+// const persistConfig = {
+//   key: "root",
+//   storage,
+//   blacklist: ["user"],
+// };
+
+// const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+// export const store = createStore(
+//   persistedReducer,
+//   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+// );
+
+// export const persistor = persistStore(store);
+//-----------------------------------------------------------------------------------------------------
+//-----------------------------------------------------------------------------------------------------
 // // import { createStore } from "redux";
 // import { compose, createStore, applyMiddleware } from "redux";
 // // import logger from "redux-logger";
@@ -38,7 +72,7 @@ export const persistor = persistStore(store);
 // const composeEnhancer =
 //   (process.env.NODE_ENV !== "/production" &&
 //     window &&
-//     window.__REDUX_DEVTOOLS_EXTENSION__COMPOSE__) ||
+//     window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) ||
 //   compose;
 
 // const composedEnhancers = composeEnhancer(applyMiddleware(...middlewares));
