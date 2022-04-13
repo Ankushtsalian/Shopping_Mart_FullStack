d8a5e3d251d52e3fb1cbe6181c2a777153e08b8d
Ankushtsalian <ankushtsalian@gmail.com>
2022-04-13 22:33:21 +0530

    added stripe provider in index

--- src/index.js
+++ src/index.js
@@ -7,12 +7,16 @@ import { BrowserRouter as Router } from "react-router-dom";
 import { Provider } from "react-redux";
 import { store, persistor } from "./store/store";
 import { PersistGate } from "redux-persist/integration/react";
+import { Elements } from "@stripe/react-stripe-js";
+import { stripePromise } from "./utils/stripe/stripe.utils";
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate persistor={persistor}>
         <Router>
-          <App />
+          <Elements stripe={stripePromise}>
+            <App />
+          </Elements>
         </Router>
       </PersistGate>
     </Provider>
