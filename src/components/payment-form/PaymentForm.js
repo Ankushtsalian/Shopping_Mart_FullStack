2fe5915ee4a2b5b594026543f49ea360a58ef4e6
Ankushtsalian <ankushtsalian@gmail.com>
2022-04-13 23:57:39 +0530

    added styles to payment form and function to payment handler

--- src/components/payment-form/PaymentForm.js
+++ src/components/payment-form/PaymentForm.js
@@ -1,12 +1,26 @@
-import { CardElement } from "@stripe/react-stripe-js";
+import { CardElement, useStripe, useElements } from "@stripe/react-stripe-js";
 import Button, { BUTTON_TYPE_CLASSES } from "../button/Button";
+import { FormContainer, PaymentFormContainer } from "./paymentForm.styles";
 
 const PaymentForm = () => {
+  const stripe = useStripe();
+  const elements = useElements();
+
+  const PaymentHandler = async (e) => {
+    e.preventDefault();
+    if (!stripe || !elements) {
+      return;
+    }
+  };
+
   return (
-    <div>
-      <CardElement />
-      <Button buttonType={BUTTON_TYPE_CLASSES.inverted}>Pay now</Button>
-    </div>
+    <PaymentFormContainer>
+      <FormContainer>
+        <h2>Credit Card Payment: </h2>
+        <CardElement />
+        <Button buttonType={BUTTON_TYPE_CLASSES.inverted}>Pay now</Button>
+      </FormContainer>
+    </PaymentFormContainer>
   );
 };
 
