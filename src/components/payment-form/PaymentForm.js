0bf5ad002db8ab76faa07d7659e9837d905ee21a
Ankushtsalian <ankushtsalian@gmail.com>
2022-04-14 12:15:45 +0530

    added stripe netlify serverless function

--- src/components/payment-form/PaymentForm.js
+++ src/components/payment-form/PaymentForm.js
@@ -1,7 +1,7 @@
 import { CardElement, useStripe, useElements } from "@stripe/react-stripe-js";
 import Button, { BUTTON_TYPE_CLASSES } from "../button/Button";
 import { FormContainer, PaymentFormContainer } from "./paymentForm.styles";
-
+// import {} from "../../../netlify/functions/create-Payment-Intent"
 const PaymentForm = () => {
   const stripe = useStripe();
   const elements = useElements();
@@ -11,11 +11,20 @@ const PaymentForm = () => {
     if (!stripe || !elements) {
       return;
     }
+    const response = await fetch("/netlify/functionscreate-Payment-Intent.js", {
+      method: "post",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ amount: 10000 }),
+    }).then((res) => res.json());
+
+    console.log(response);
   };
 
   return (
     <PaymentFormContainer>
-      <FormContainer>
+      <FormContainer onSubmit={PaymentHandler}>
         <h2>Credit Card Payment: </h2>
         <CardElement />
         <Button buttonType={BUTTON_TYPE_CLASSES.inverted}>Pay now</Button>
