07df3eb6f14c9181e7368009ca619e74ce154d6f
Ankushtsalian <ankushtsalian@gmail.com>
2022-04-15 10:37:29 +0530

    updated logo

--- src/components/cart-dropdown/CartDropdown.js
+++ src/components/cart-dropdown/CartDropdown.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { useCallback } from "react";
 
 import { useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
@@ -11,9 +12,10 @@ const CartDropdown = () => {
   const cartItems = useSelector(selectCartItems);
 
   const navigate = useNavigate();
-  const goToCheckoutHandler = () => {
+
+  const goToCheckoutHandler = useCallback(() => {
     navigate("/checkout");
-  };
+  }, [navigate]);
   return (
     <div className="cart-dropdown-container">
       <div className="cart-items">
